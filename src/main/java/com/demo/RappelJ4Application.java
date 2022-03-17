72913e048552b3d04fba9a2249fd332cab55ba27
khalil <khalillakhdharatc@gmail.com>
2022-03-17 11:17:25 +0100

    Initial commit

---
+++ src/main/java/com/demo/RappelJ4Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RappelJ4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RappelJ4Application.class, args);
+	}
+
+}
